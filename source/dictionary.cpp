64a87eb5c09b8355b8369401b7ef091c6b8bab12
Kirill Berezin <enelar@develop-project.ru>
2013-04-11 13:37:21 +0400

    Base dictionary functional

---
+++ source/dictionary.cpp
@@ -0,0 +1,33 @@
+/* This file protected by faith_base_00 license
+ * No part of this file could be copyed or changed without Kirill Berezin agreement
+ * No part of this file could be used or viewed without Kirill Berezin agreement
+ */
+
+#include "dictionary.h"
+
+using namespace ed;
+
+dictionary::id_type dictionary::SearchWord( word_type word ) const
+{
+  id_type i = 0, s = data_base.size();
+  for (;i < s; ++i)
+    if (data_base[i] == word)
+      return i;
+  return reserved_as_failure;
+}
+
+dictionary::id_type dictionary::RegisterWord( word_type word )
+{
+  id_type ret = SearchWord(word);
+  if (ret == reserved_as_failure)
+  {
+    data_base.push_back(word);
+    ret = data_base.size();
+  }
+  return ret;
+}
+
+bool dictionary::IsWordExist( word_type word ) const
+{
+  return SearchWord(word) != reserved_as_failure;
+}
---
+++ source/dictionary.h
@@ -0,0 +1,28 @@
+/* This file protected by faith_base_00 license
+ * No part of this file could be copyed or changed without Kirill Berezin agreement
+ * No part of this file could be used or viewed without Kirill Berezin agreement
+ */
+
+#ifndef _ED_DICTIONARY_H_
+#define _ED_DICTIONARY_H_
+
+#include <vector>
+#include <string>
+
+namespace ed
+{
+  struct dictionary
+  {
+    typedef std::string word_type;
+    typedef unsigned int id_type;
+  private:
+    std::vector<word_type> data_base;
+    static const id_type reserved_as_failure = 0;
+    id_type SearchWord( word_type ) const;
+  public:
+    id_type RegisterWord( word_type );
+    bool IsWordExist( word_type ) const;
+  };
+};
+
+#endif
