b409491a663da84a511bcf061d8f32ff2ee30add
Kirill Berezin <enelar@develop-project.ru>
2013-04-15 21:26:38 +0400

    First successful server build

--- source/server/main.cpp
+++ source/server/main.cpp
@@ -1,5 +1,11 @@
+#include "../ed/controllers/server_controller.h"
+#include "../ed/communications/socket_ready.h"
 
+using namespace ed;
+using namespace ed::com;
 
 void main()
 {
+  socket_ready *port = NEW socket_ready(3030);
+  server_controller<socket_ready> s(port);
 }
