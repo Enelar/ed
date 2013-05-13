48e3aeda01b81485a560d09f49a9454c07548c12
Kirill Berezin <enelar@develop-project.ru>
2013-05-13 07:10:29 +0400

    Filled notification struct

--- source/ed/messages/notification.h
+++ source/ed/messages/notification.h
@@ -1,11 +1,17 @@
 #ifndef _ED_MESSAGES_NOTIFICATION_H_
 #define _ED_MESSAGES_NOTIFICATION_H_
 
+#include "../notifications/event_notification.h"
+
 namespace ed
 {
-  class notification_message
+  struct notification_message : public event_notification
   {
-  
+  public:
+    register_message( message &e )
+    {
+      throw_assert(e.GetType() == NOTIFY);
+    }
   };
 };
 
