607d28fa74e7e3cf63dc4b66a6d36247424946c5
Kirill Berezin <enelar@develop-project.ru>
2013-08-09 23:24:06 +0400

    New format of the event handler

--- source/ed/kit/module_callback.cpp
+++ source/ed/kit/module_callback.cpp
@@ -2,7 +2,7 @@
 
 using namespace ed;
 
-void module::RegisterQueryCallback( query_callback_type q, std::string event, std::string module, int source_instance )
+void module::SetPreEventHandler( pre_event_handler_t q, std::string event, std::string module, int source_instance )
 {
   int module_global_id = gw.RegisterName(MODULES, module);
   int event_global_id = gw.RegisterName(EVENTS, event);
@@ -11,19 +11,19 @@ void module::RegisterQueryCallback( query_callback_type q, std::string event, st
   es.event = event_global_id;
   es.module = module_global_id;
   es.instance = source_instance;
-  RegisterQueryCallback(q, es);
+  SetPreEventHandler(q, es);
 }
 
-void module::RegisterQueryCallback( query_callback_type q, event_source source )
+void module::SetPreEventHandler( pre_event_handler_t q, event_source source )
 {
-  callback_entry<query_callback_type> t;
+  callback_entry<pre_event_handler_t> t;
   t.callback = q;
   t.source = source;
   QueryCallbacks.push_back(t);
   gw.Listen(source.instance, id, source.module, source.event);
 }
 
-void module::RegisterEventCallback( event_callback_type q, std::string event, std::string module, int source_instance )
+void module::SetPostEventHandler( post_event_handler_t q, std::string event, std::string module, int source_instance )
 {
   int module_global_id = gw.RegisterName(MODULES, module);
   int event_global_id = gw.RegisterName(EVENTS, event);
@@ -32,12 +32,12 @@ void module::RegisterEventCallback( event_callback_type q, std::string event, st
   es.event = event_global_id;
   es.module = module_global_id;
   es.instance = source_instance;
-  RegisterEventCallback(q, es);
+  SetPostEventHandler(q, es);
 }
 
-void module::RegisterEventCallback( event_callback_type q, event_source source )
+void module::SetPostEventHandler( post_event_handler_t q, event_source source )
 {
-  callback_entry<event_callback_type> t;
+  callback_entry<post_event_handler_t> t;
   t.callback = q;
   t.source = source;
   EventCallbacks.push_back(t);
@@ -50,12 +50,12 @@ void module::EventReciever( const message &m )
   const event_source en = static_cast<event_notification>(m);
   event_source en0 = en;
   en0.event = reserved::event::BROADCAST;
-  std::vector<callback_entry<event_callback_type>>::const_iterator
+  std::vector<callback_entry<post_event_handler_t>>::const_iterator
     i = EventCallbacks.begin(),
     e = EventCallbacks.end();
   while (i != e)
   {
-    const callback_entry<event_callback_type> &t = *i++;
+    const callback_entry<post_event_handler_t> &t = *i++;
     if (en == t.source || en0 == t.source)
       (this->*t.callback)(event_context(adapter.ToLocal(m.event), en, m.payload));
   }
@@ -64,12 +64,12 @@ void module::EventReciever( const message &m )
 bool module::Query( const message &m )
 {
   const event_notification en = m;
-  std::vector<callback_entry<query_callback_type>>::const_iterator
+  std::vector<callback_entry<pre_event_handler_t>>::const_iterator
     i = QueryCallbacks.begin(),
     e = QueryCallbacks.end();
   while (i != e)
   {
-    const callback_entry<query_callback_type> &t = *i++;
+    const callback_entry<pre_event_handler_t> &t = *i++;
     if (en.source == t.source)
       if (!(this->*t.callback)(event_context(adapter.ToLocal(m.event), en.source, m.payload)))
         return false;
