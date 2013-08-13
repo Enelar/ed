d55d448ca78e171aba68305f716572f51a15536f
Kirill Berezin <enelar@develop-project.ru>
2013-08-13 04:44:39 +0400

    Fix little memory leak

--- source/server/main.cpp
+++ source/server/main.cpp
@@ -10,15 +10,15 @@ using namespace ed::com;
 
 void f()
 {
-  socket_ready *port = NEW socket_ready(3030);
-  server_controller s(*port);
+  socket_ready port(3030);
+  server_controller s(port);
   socket_ready::pair_type *socket = NEW socket_ready::pair_type("localhost", 3030);
   client_controller<socket_ready::pair_type> c(socket);
 
   while (1)
   {
     Sleep(1);
-      s.Workflow();
+    s.Workflow();
   }
 }
 
