0293a43530208c9a591481b8815dd9a49aa00513
Kirill Berezin <enelar@develop-project.ru>
2013-08-15 04:29:31 +0400

    Fix self-notify bug

--- source/ed/notifications/listener.h
+++ source/ed/notifications/listener.h
@@ -1,6 +1,9 @@
 #ifndef _ED_NOTIFICATIONS_LISTENER_H_
 #define _ED_NOTIFICATIONS_LISTENER_H_
 
+#include "../exceptions/exception.h"
+#include "event_source.h"
+
 namespace ed
 {
   namespace slot_data
@@ -11,6 +14,11 @@ namespace ed
 
       bool operator==( const listener &a ) const
       {
+        return instance == a.instance && module == a.module;
+      }
+      bool operator==( const event_source &a ) const
+      {
+        throw_assert(a.instance && a.module);
         return instance == a.instance && module == a.module;
       }
     };
