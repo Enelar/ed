e59bd68bc161c85734e01946eb7f93f0aa6d8aee
Kirill Berezin <enelar@develop-project.ru>
2013-08-18 06:45:46 +0400

    Reorganize declare order

--- source/ed/kit/module.h
+++ source/ed/kit/module.h
@@ -20,15 +20,7 @@ namespace ed
 
     typedef module_impl::base_pre_callback_entry base_pre_callback_entry;
     typedef module_impl::base_post_callback_entry base_post_callback_entry;
-  private:
 
-    module_impl &impl;
-    friend class gateway;
-    friend class gateway_impl;
-    friend class event_result;
-    module( int id, gateway & );
-    bool SendPreEvent( int local_id, message & );
-    void SendPostEvent( int local_id, message & );
   public:
     module( const std::string &, gateway & );
     virtual ~module();
@@ -44,10 +36,7 @@ namespace ed
       int local_id,
       EVENT_RING query_max_ring = RING0_THREAD,
       EVENT_RING notify_max_ring = RING3_WORLD );
-      
 
-
-  private:
   protected:
     template<typename T, typename MODULE>
     void RegisterPreHandler( bool (MODULE::*f)( const event_context<T> & ), event_source es );
@@ -60,12 +49,22 @@ namespace ed
 
     template<typename MODULE>
     void UnregisterHandlers( const MODULE *const );
+
   private:
     template<typename T, typename MODULE, typename RET>
     module_impl::callback_entry<typename RET> *SysCreateHandler( RET (MODULE::*f)( const event_context<T> & ), event_source es );
 
     void EventReciever( const message & );
     bool Query( const message & );
+
+    friend class gateway;
+    friend class gateway_impl;
+    friend class event_result;
+    module( int id, gateway & );
+    bool SendPreEvent( int local_id, message & );
+    void SendPostEvent( int local_id, message & );
+
+    module_impl &impl;
   };
 };
 
