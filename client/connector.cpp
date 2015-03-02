15a6cc83fb09cc4c142eb8e7a2436da8be01e0c9
Kirill Berezin <enelar@develop-project.ru>
2015-03-03 02:38:13 +0300

    Connector actually trying to do something

--- client/connector.cpp
+++ client/connector.cpp
@@ -1,6 +1,7 @@
 #define _ED_INTERNAL_CONNECTOR_NOCHECK_
 #include "connector.h"
 #include <ed/structs/messages/simple_messages.h>
+#include <boost/lexical_cast.hpp>
 
 connector::connector(boost::asio::io_service &_io)
   : io(_io), con(io)
@@ -10,19 +11,47 @@ connector::connector(boost::asio::io_service &_io)
 
 void connector::Connect(string addr, int port)
 {
+  using namespace boost::asio::ip;
 
+  tcp::resolver resolver(io);
+  tcp::resolver::query query(addr, boost::lexical_cast<string>(port));
+  tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+  tcp::resolver::iterator end;
+
+  boost::system::error_code error = boost::asio::error::host_not_found;
+  while (error && endpoint_iterator != end)
+  {
+    con.close();
+    con.connect(*endpoint_iterator++, error);
+  }
+  if (error)
+    throw boost::system::system_error(error);
 }
 
 void connector::SendMessage(raw_message gift)
 {
   gift.from.instance = global_instance_id;
-  // TODO: Boost send messages
+  vector<byte> buf = gift;
+  con.send(boost::asio::buffer(buf));
 }
 
 raw_message connector::WaitForMessage()
 {
-  // TODO: Boost recieve message
-  raw_message gift;
+  vector<byte> buf;
+
+  {
+    const int sizeof_payload_size = 4;
+    const int to_read = message_header::raw_byte_size + 4;
+    buf.reserve(to_read); // size of payload
+    boost::asio::read(con, boost::asio::buffer(buf, to_read));
+    int payload_size = *(int *)(&buf[0] + message_header::raw_byte_size);
+    if (payload_size > raw_message::max_message_size)
+      throw "reading message is too big";
+    buf.reserve(buf.size() + payload_size);
+    boost::asio::read(con, boost::asio::buffer(&buf[0] + to_read, payload_size));
+  }
+
+  raw_message gift = &buf[0];
   if (gift.from.instance == ed::reserved::instance::CONTROLLER)
     if (gift.to.instance != ed::reserved::instance::BROADCAST)
       global_instance_id = gift.to.instance;
@@ -32,7 +61,7 @@ raw_message connector::WaitForMessage()
 int connector::RegisterName(bool is_event, string name)
 {
   {
-    messages::_string request;
+    ::messages::_string request;
     request.to.instance = ed::reserved::instance::CONTROLLER;
     request.to.module = ed::reserved::module::BROADCAST;
     request.from.module = ed::reserved::module::BROADCAST;
@@ -45,7 +74,7 @@ int connector::RegisterName(bool is_event, string name)
     SendMessage(request);
   }
 
-  messages::_int response = WaitForMessage();
+  ::messages::_int response = WaitForMessage();
   int global_id = response.payload.num;
 
   if (is_event)
